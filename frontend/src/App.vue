5436c13859c1ff000b9380c42e3127231cc5aee6
Adrian <adriantomas@protonmail.com>
2019-04-02 21:10:36 +0200

    added navbar, elements WIP

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <v-app dark>
-    <v-toolbar app>
+    <v-navigation-drawer app clipped right>
+      <div>
+        <span class="title">Periódicos</span>
+        <v-switch
+          v-for="newspaper in periodicos"
+          v-model="selectedNewspapers"
+          :value="newspaper"
+          :key="newspaper"
+          :label="newspaper"
+        ></v-switch>
+      </div>
+      <div>
+        <span class="title">Fecha</span>
+        <v-date-picker 
+          v-model="dateSelected"
+          first-day-of-week="1"
+          locale="es-es"
+          reactive
+          no-title
+          :max="today">
+        </v-date-picker>
+      </div>
+    </v-navigation-drawer>
+
+    <v-toolbar app fixed clipped-right>
       <v-toolbar-title class="headline text-uppercase">
         <span>contrast</span>
         <span class="font-weight-light"></span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-text-field
-      prepend-inner-icon="search"
-      >
-      </v-text-field>
+      <v-text-field prepend-inner-icon="search"></v-text-field>
       <v-btn icon class="hidden-xs-only">
         <v-icon>md-dropdown</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-toolbar>
+
     <v-content>
-     <!-- <v-expansion-panel 
+      <!-- <v-expansion-panel 
       v-model="panel"
       expand>
     <v-expansion-panel-content>
@@ -40,20 +58,28 @@
         <v-icon>search</v-icon>
       </v-btn>
     </v-expansion-panel-content>
-    </v-expansion-panel>  -->
+      </v-expansion-panel>-->
       <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import moment from 'moment'
 
 export default {
-  name: 'App',
-  data () {
-    return {
-      //
+  name: "App",
+  data: () => ({
+    periodicos: ["eldiario", "elmundo", "elpais"],
+    selectedNewspapers: ["eldiario", "elmundo", "elpais"],
+    dateSelected: new Date().toISOString().substr(0,10),
+    today: new Date().toISOString().substr(0,10)
+  }),
+
+  computed: {
+    formatDate () {
+      return this.dateSelected ? moment(this.dateSelected).format('DD/MM/YYYY') : ''
     }
   }
-}
+};
 </script>
--- frontend/src/components/NewsList2.vue
+++ frontend/src/components/NewsList2.vue
@@ -16,37 +16,40 @@
       <v-flex v-for="i in 3" :key="`4${i}`" xs4>
         <!-- <v-card color="primary" hover :href="wwww.google.es">
           <v-card-text class="px-0">{{ j+ i}}</v-card-text>
-        </v-card> -->
+        </v-card>-->
 
         <v-card class="ma-3">
-        <v-card-title>   
-          <!-- primary-title -->        
+          <v-card-title>
+            <!-- primary-title -->
             <span class="headline">Titular</span>
             <v-spacer></v-spacer>
             <div>
-              <span class="grey--text">Fecha</span><br>
+              <span class="grey--text">Fecha</span>
+              <br>
               <span class="grey--text">Periódico</span>
             </div>
-        </v-card-title>
-      <v-slide-y-transition>
-        <v-card-text :class="{'text-truncate': show}" class="black--text white text-xs-left" height="60%"> 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam massa, vehicula quis vulputate quis, auctor non lectus. Mauris vitae erat eu sapien lacinia dictum ut quis leo. Vivamus gravida est non nulla dictum, vitae gravida lacus efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ac imperdiet erat, at sagittis lorem. Suspendisse consequat dolor at augue hendrerit vestibulum. Nam ut interdum ligula, sit amet aliquet quam. In tempus ornare velit nec mattis. Pellentesque libero eros, malesuada ac scelerisque quis, lobortis at nibh. Maecenas at dui augue
-        </v-card-text>
-</v-slide-y-transition>
-        <v-card-actions>
-          <v-btn flat>Ampliar</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
+          </v-card-title>
+          <v-slide-y-transition>
+            <v-card-text
+              :class="{'text-truncate': show}"
+              class="black--text white text-xs-left"
+              height="60%"
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris diam massa, vehicula quis vulputate quis, auctor non lectus. Mauris vitae erat eu sapien lacinia dictum ut quis leo. Vivamus gravida est non nulla dictum, vitae gravida lacus efficitur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ac imperdiet erat, at sagittis lorem. Suspendisse consequat dolor at augue hendrerit vestibulum. Nam ut interdum ligula, sit amet aliquet quam. In tempus ornare velit nec mattis. Pellentesque libero eros, malesuada ac scelerisque quis, lobortis at nibh. Maecenas at dui augue</v-card-text>
+          </v-slide-y-transition>
+          <v-card-actions>
+            <v-btn flat>Ampliar</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="show = !show">
+              <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
 
-        <!-- <v-slide-y-transition>
+          <!-- <v-slide-y-transition>
           <v-card-text v-show="show">
             I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
           </v-card-text>
-        </v-slide-y-transition> -->
-      </v-card>
+          </v-slide-y-transition>-->
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -57,5 +60,5 @@ export default {
   data: () => ({
     show: false
   })
-}
+};
 </script>
